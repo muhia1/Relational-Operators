bb6fbc0ba8bf29e32b129678f9391d700d212303
Subeh <muhiadylan@gmail.com>
2025-01-29 14:03:46 +0300

    Add files via upload

---
+++ RelationalOperators.java
@@ -0,0 +1,25 @@
+// Github : Muhia1
+// Relational Operators-This compares two values and determines the relationship between them for a given condition
+public class RelationalOperators {
+    public static void main(String []args){
+        
+        int i= 37;
+        int j= 42;
+        int k= 42;
+
+        System.out.println("The Variables:");
+        System.out.println("i=" +i);
+        System.out.println("j=" +j);
+        System.out.println("k=" +k);
+
+        //Equals
+        System.out.println("i==j " +(i==j));
+        System.out.println("j==k " +(j==k));
+        //Not Equals
+        System.out.println("i!=j " +(i!=j));
+        System.out.println("j!=k " +(j!=k));
+        //Less than
+        System.out.println("i<j " +(i<j));
+        System.out.println("j<k " +(j<k));
+    }
+}
